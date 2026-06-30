eea639ff9832eedf196720bb357a41c5b49181a3
José Luis Undurraga <undurragamorales@gmail.com>
2026-06-29 20:41:40 -0400

    feat: create command-line client for AI Labs assistant
    - Added a lightweight CLI for interacting with the assistant.
    - Connected the console interface to the RAG pipeline.
    - Enabled local testing without the web interface.
    - Kept the application entry point separated from the backend.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
-print("=" * 50)
-print("Oracle ONE AI Challenge")
-print("=" * 50)
+from dotenv import load_dotenv
+from src.agent import ask_agent
 
-print()
+load_dotenv()
 
-print("🐳 My first Docker image is running!")
+
+
+
+def main():
+
+    print("=" * 50)
+    print("Oracle ONE AI Challenge")
+    print("=" * 50)
+    print()
+
+    print("🤖 Hola, soy Laby, el asistente virtual de IA Labs. Recuerda que puedes escribir salir para terminar la conversación")
+    print("🤖 Laby: Antes de comenzar, ¿cómo te llamas?")
+    name = input("👤 Tú: ").strip()
+    print(f"\n🤖 Laby: Mucho gusto, {name}. ¿En qué puedo ayudarte?\n")
+    #print("Escribe 'salir' para terminar.\n")
+
+    while True:
+
+        question = input("👤 Tú: ").strip()
+
+        if question.lower() == "salir":
+            print("\n🤖 Laby: ¡Hasta luego!")
+            break
+        
+        answer = ask_agent(name, question)
+
+        print(f"\n🤖 Laby: {answer}\n")
+
+
+if __name__ == "__main__":
+    main()
